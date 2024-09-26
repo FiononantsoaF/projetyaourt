a344f865e614a16a2a57e0855e5498a00a879529
Andriamiandry <155827763+Andriamiandry@users.noreply.github.com>
2024-09-26 18:03:13 +0200

    Here

---
+++ Yaourt/application/views/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Welcome to the Login</h3>
+    <p style="color: red;">Mandeh</p>
+</body>
+</html>
