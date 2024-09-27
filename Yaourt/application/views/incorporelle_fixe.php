a741d666282ba09767915ed8f430179c8709c44c
Andriamiandry <155827763+Andriamiandry@users.noreply.github.com>
2024-09-27 18:17:53 +0200

    Page d'insertion

---
+++ Yaourt/application/views/incorporelle_fixe.php
@@ -0,0 +1,19 @@
+<p>Charge de nature fixe :</p>
+<div class="form-check">
+    <input class="form-check-input" type="radio" name="chargeFixe" id="option1" value="Option 1">
+    <label class="form-check-label" for="option1">
+        Option 1
+    </label>
+</div>
+<div class="form-check">
+    <input class="form-check-input" type="radio" name="chargeFixe" id="option2" value="Option 2">
+    <label class="form-check-label" for="option2">
+        Option 2
+    </label>
+</div>
+<div class="form-check">
+    <input class="form-check-input" type="radio" name="chargeFixe" id="option3" value="Option 3">
+    <label class="form-check-label" for="option3">
+        Option 3
+    </label>
+</div>
---
+++ Yaourt/application/views/incorporelle_variable.php
@@ -0,0 +1,19 @@
+<p>Charge de nature variable :</p>
+<div class="form-check" style="display: flex; align-items: center; margin-bottom: 10px;">
+    <input class="form-check-input" type="checkbox" name="chargeVariable[]" id="option1" value="Option 1">
+    <label class="form-check-label" for="option1" style="margin-right: 10px;">
+        Option 1
+    </label>
+    <div class="wave-group" style="flex: 1;">
+        <input required="" type="number" class="input" style="width: 100px;">
+    </div>
+</div>
+<div class="form-check" style="display: flex; align-items: center; margin-bottom: 10px;">
+    <input class="form-check-input" type="checkbox" name="chargeVariable[]" id="option2" value="Option 2">
+    <label class="form-check-label" for="option2" style="margin-right: 10px;">
+        Option 2
+    </label>
+    <div class="wave-group" style="flex: 1;">
+        <input required="" type="number" class="input" style="width: 100px;">
+    </div>
+</div>
