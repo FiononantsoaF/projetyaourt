da490ab624542eb7c145245b9364ce7e59872a4e
RebecaRakotomalala <ranjarakoto0@gmail.com>
2024-09-28 07:47:07 +0300

    grand tableau modifier

--- base/view.sql
+++ base/view.sql
@@ -22,3 +22,30 @@ JOIN
     centre c ON r.idCentre = c.idCentre
 JOIN
     nature n ON r.idNature = n.idNature;
+
+CREATE OR REPLACE VIEW vueRepartitionAvecCalculs AS
+SELECT
+    cg.idCharge,
+    cg.nomCharge,
+    cg.montant,
+    cg.daty,
+    r.idRepartition,
+    c.idCentre,
+    c.nomCentre,
+    u.idUnite,
+    u.nomUnite,
+    n.idNature,
+    n.nomNature,
+    r.pourcentage,
+    CASE WHEN n.nomNature = 'Fixe' THEN (r.pourcentage / 100) * cg.montant ELSE 0 END AS montantFixe,
+    CASE WHEN n.nomNature = 'Variable' THEN (r.pourcentage / 100) * cg.montant ELSE 0 END AS montantVariable
+FROM
+    chargeGeneral cg
+JOIN
+    repartition r ON cg.idCharge = r.idGeneral
+JOIN
+    uniteOeuvre u ON r.idUnite = u.idUnite
+JOIN
+    centre c ON r.idCentre = c.idCentre
+JOIN
+    nature n ON r.idNature = n.idNature;
